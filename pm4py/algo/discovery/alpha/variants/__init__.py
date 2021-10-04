5149cf40f5c07f78545977a88c5d4a481ae4810c
pulkit604 <pulkit.gupta@stud.hs-ruhrwest.de>
2021-10-04 23:51:32 +0200

    variant import

--- pm4py/algo/discovery/alpha/variants/__init__.py
+++ pm4py/algo/discovery/alpha/variants/__init__.py
@@ -14,4 +14,4 @@
     You should have received a copy of the GNU General Public License
     along with PM4Py.  If not, see <https://www.gnu.org/licenses/>.
 '''
-from pm4py.algo.discovery.alpha.variants import classic, plus
+from pm4py.algo.discovery.alpha.variants import classic, plus, alphaf
