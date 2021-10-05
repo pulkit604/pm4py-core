0041a8873d1fc875d276908dbba91963d6ddaaa7
pulkit604 <guppul@gmail.com>
2021-10-06 01:05:38 +0200

    more debug

--- pm4py/algo/discovery/alpha/variants/alphaf.py
+++ pm4py/algo/discovery/alpha/variants/alphaf.py
@@ -331,7 +331,9 @@ def processing(log: EventLog, causal: Tuple[str, str], follows: Tuple[str, str])
             label_transition_dict[label] = PetriNet.Transition(label, None)
             net.transitions.add(label_transition_dict[label])
     # and source and sink
-    src = add_source(net, start_activities.add('navigation get premium'), label_transition_dict)
+    print(type(start_activities), start_activities)
+    print(type(start_activities.add('navigation get premium')), start_activities.add('navigation get premium'))
+    src = add_source(net, start_activities, label_transition_dict)
     sink = add_sink(net, end_activities, label_transition_dict)
     print(net, start_activities, label_transition_dict, end_activities)
     # create places
